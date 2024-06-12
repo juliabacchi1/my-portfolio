0d1bea099d55e43da492ffc1f627150d4fe01b71
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-06-12 17:31:12 -0300

    Initial commit

---
+++ src/style.js
@@ -0,0 +1,41 @@
+// rolagem suave quando um desses links é clicado
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
+
+// mostrar/esconder o botão
+const backToTopButton = document.getElementById("back-to-top");
+
+window.onscroll = function () {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.remove("hide");
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+    backToTopButton.classList.add("hide");
+  }
+};
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// GSAP animation
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("JavaScript carregado!");
+  gsap.to("#logo", {
+    rotation: 360,
+    duration: 2,
+    repeat: -1,
+    ease: "linear",
+  });
+});
