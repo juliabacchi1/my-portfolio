164dc594ce21c232e93bf74783654aec76ecc1a9
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-08-30 17:43:55 -0300

    Added a content for the blog: manuals, images and new post

--- src/blog.js
+++ src/blog.js
@@ -2,6 +2,31 @@
 
 const articles = [
   {
+    title:
+      "The Guide That Was Missing: How I Created Simplifying Programming for Beginners",
+    content: `<h4>Bridging the Gap</h4>
+    <p class="text-justify mt-4">Learning to program has never been more accessible than it is today. With the amount of content available across educational platforms, lack of information is no longer a valid excuse.
+    <br /><br />SheCodes, for example, was so fundamental in my journey, with so important courses that shaped my knowledge in the area. Likewise, there are other renowned institutions that provide high-quality materials to train great web developers.
+    <br /><br />However, I identified a significant gap: the absence of content that is both direct and comprehensive, capable of introducing the world of web development in a clear and accessible way for those just starting out in programming. Many people wonder if this is really their next career and, before they commit to paid courses, they need a way to genuinely gauge their interest.</p>`,
+    content2: `<h4 class="m-0">Simplifying Programming - Beginner</h4>
+<br /> <p class="text-justify">Thinking about this need, I created Simplifying Programming: The material that was carefully designed to bring together, in a clear and efficient way, the fundamentals of the main programming languages. The goal is to be able to provide an initial reference for those just starting out, providing a solid and practical foundation to embark on their web development journey.
+<br /><br />With Simplifying Programming, my commitment is to make knowledge accessible and help transform the way people perceive programming. I'm here to be part of this global change and to inspire and help people decide their next steps, so they feel safe and confident to moving forward.
+<br /><br /><strong>Introduction to the Manual</strong>
+<p class="text-justify mt-4">Welcome to your programming guide! This manual was created to help beginners take their first steps in the world of web development. Here, you'll find clear and practical explanations about HTML, CSS, and JavaScript, the three essential languages for anyone who wants to create functional and attractive websites and applications.</p>
+<strong>The Importance of Starting with HTML, CSS, and JavaScript</strong>
+<p class="text-justify mt-4">HTML, CSS, and JavaScript are the foundations of the web. HTML is responsible for structuring content, CSS handles the look and style, and JavaScript adds interactivity and dynamism. Understanding these three languages is essential for any front-end developer, and they are the perfect starting point for beginners.</p>
+<strong>Purpose and Structure of the Material</strong>
+<p class="text-justify mt-4">The goal of this manual is to guide you, step by step, through the essential concepts and practices of these languages. Each section of the manual is dedicated to one of these technologies, with practical examples to make learning easier. By the end, you'll have a solid foundation to continue exploring the programming world and creating your own projects.</p>
+<strong>Complete Manual Available for Download</strong>
+<p class="text-justify mt-4">To make your learning experience even more convenient, the complete manual is available in a website and PDF versions, in English and Portuguese. You can download it and study offline at your own pace.</p>
+<p>Open the Manual here: <a href="https://simplifying-2qpqfs7.gamma.site/" target="_blank">Simplyfying Programming - Beginner</a>
+<br /><br /><a href="imgs/descomplicando-manual.pdf" target="_blank">Download Manual in Portuguese (PDF)</a>
+<br /><a href="imgs/simplifying-manual.pdf" target="_blank">Download Manual in English (PDF)</a>
+</p>`,
+    date: "August 30, 2024",
+    image: "imgs/simplifying.png",
+  },
+  {
     title: "The importance and challenges of career change",
     content: `<h4>Why is it difficult to change?</h4>
 <p class="text-justify mt-4">Changing careers is a significant decision that many people face. Whether due to job dissatisfaction or a desire for purpose, this transformation comes with challenges that require preparation and perseverance. 
@@ -15,7 +40,7 @@ const articles = [
 <br /><br />  I continually question my path but remain committed to discovering where I truly belong in the tech world. Currently, I'm working on projects that expand my knowledge, and I'm excited about future possibilities. 
   My advice for others is to fearlessly pursue their dreams; with determination and dedication, any dream can become a reality.
 </p>`,
-    date: "2024-08-01",
+    date: "July 28, 2024",
     image: "imgs/butterfly.jpg",
   },
   // Adicione mais artigos aqui
